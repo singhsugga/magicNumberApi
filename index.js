48640de6caf5c1bdd6acbb59a1e0ac3197c8d12d
singhsugga <singhsugga@outlook.com>
2019-07-25 19:31:50 +0530

    dependency adjusted

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const app = express()
+var cors = require('cors')
+app.use(cors())
+app.get('/number', (req, res) => {
+
+let number= {number: Math.floor(100000 + Math.random() * 900000)}
+
+                    setTimeout(function() {
+                        res.json(number);
+                    }, 10000);
+}); //Line 25
+
+
+app.listen(3000, () => console.log('Server Started on localhost:3000'))
