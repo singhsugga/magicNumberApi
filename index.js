89bfbb8ee41bc5ab45b89a5ba0dafd42fa800164
singhsugga <singhsugga@outlook.com>
2019-07-25 19:41:13 +0530

    sc

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express')
 const app = express()
 var cors = require('cors')
 app.use(cors())
+app.get('', (req, res) => {
+
+    let number= {number: Math.floor(100000 + Math.random() * 900000)}
+    
+                        setTimeout(function() {
+                            res.json(number);
+                        }, 10000);
+    });
 app.get('/number', (req, res) => {
 
 let number= {number: Math.floor(100000 + Math.random() * 900000)}
@@ -10,7 +18,7 @@ let number= {number: Math.floor(100000 + Math.random() * 900000)}
                     setTimeout(function() {
                         res.json(number);
                     }, 10000);
-}); //Line 25
+});
 
 
 app.listen(3000, () => console.log('Server Started on localhost:3000'))
