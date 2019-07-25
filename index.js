d781cb32aa825d59d0595377ec98fb0cf1e2c03c
singhsugga <singhsugga@outlook.com>
2019-07-25 19:43:37 +0530

    sds

--- index.js
+++ index.js
@@ -21,4 +21,4 @@ let number= {number: Math.floor(100000 + Math.random() * 900000)}
 });
 
 
-app.listen(3000, () => console.log('Server Started on localhost:3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Server Started on localhost:3000'))
